bf0d4a37362ab5e4ee5b9735f3da5b50f44d39eb
Charlie Wei <91643249+charliewei0716@users.noreply.github.com>
2023-02-17 12:09:57 +0800

    Update verify.sh

--- fastapi-deployment/step2/verify.sh
+++ fastapi-deployment/step2/verify.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-result=$( docker inspect -f {{.State.Running}} registry)
+container_running = $( docker inspect -f {{.State.Running}} registry)
 
-if [ $result == "true" ]
-then
-  exit 0
-else
-  exit 1
-fi
+if [ $container_running != "true" ]; then exit 1; fi
+
+fastapi-helloworl_in_registry =  $(curl http://localhost:5000/v2/_catalog | jq -c '.["repositories"] as $repos | "fastapi-helloworld" | IN($repos[])')
+
+if [ $container_running == "true" ]; then exit 0; else exit 1; fi
