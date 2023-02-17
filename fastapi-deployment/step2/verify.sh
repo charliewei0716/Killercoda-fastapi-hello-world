a1bbb08110697c88f9aee08fb97339882a54a89d
Charlie Wei <91643249+charliewei0716@users.noreply.github.com>
2023-02-17 12:19:26 +0800

    Update verify.sh

--- fastapi-deployment/step2/verify.sh
+++ fastapi-deployment/step2/verify.sh
@@ -6,4 +6,4 @@ if [ $container_running != "true" ]; then exit 1; fi
 
 fastapi-helloworl_in_registry =  $(curl http://localhost:5000/v2/_catalog | jq -c '.["repositories"] as $repos | "fastapi-helloworld" | IN($repos[])')
 
-if [ $container_running == "true" ]; then exit 0; else exit 1; fi
+if [ $helloworl_in_registry == "true" ]; then exit 0; else exit 1; fi
